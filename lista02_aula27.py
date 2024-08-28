b9ddf21a414e70e24df381e4c1337e3fd7bf8052
yelalopez <yelitzalopezduque@gmail.com>
2024-08-28 11:24:06 -0300

    Exercicios extras aula 27/08

---
+++ lista02_aula27.py
@@ -0,0 +1,13 @@
+numero_caixoes = 0
+total = 0
+
+while True:
+
+  numero_caixoes = int(input('Insira o código do caixão: '))
+  
+  if numero_caixoes == -1:
+    break
+  
+  total += 1
+  
+print(f'Numero de caixões cadastrados: {total}')
