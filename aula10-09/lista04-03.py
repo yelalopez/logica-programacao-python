6acf7d5d880d06fb1a8f58440511565d02f1d855
yelalopez <yelitzalopezduque@gmail.com>
2024-09-23 09:21:57 -0300

    fix: exercicio 03

--- aula10-09/lista04-03.py
+++ aula10-09/lista04-03.py
@@ -1,32 +1,15 @@
-# def sequencia():
-#   while True:
-#     value = int(input('Digite um número inteiro ou 0 para sair: '))
-#     if value == 0:
-#       break
-#     elif value > 0:
-#       sequencia = ' '.join(str(value) for value in range(1, value + 1))
-#       print(sequencia)
-#     else:
-#       print('Insira um inteiro positivo')
-
-
-def sequencia(lista):
-   for x in lista:
-    i = 1
-    while i<=x:
-      print(i, end=' ')
-      i+=1
-    print()
-
-lista = []
-
-while True:
-  value = int(input('Digite um número inteiro ou 0 para sair: '))
-  if value == 0:
+def sequencia():
+  while True:
+    value = int(input('Digite um número inteiro ou 0 para sair: '))
+    if value == 0:
       break
-  elif value < 0:
-    print('Insira um inteiro positivo')
-  else:
-    lista.append(value)
+    elif value > 0:
+      sequencia = ' '.join(str(value) for value in range(1, value + 1))
+      print(sequencia)
+    else:
+      print('Insira um inteiro positivo')
 
-sequencia(lista)
+   
+
+  
+sequencia()
